b7b6d8621446ff32d6b88662256e010c623833af
Dev-JeanSantos <jeancbsan@gmail.com>
2021-05-03 09:42:25 -0300

    validating fields to create an author

--- src/main/kotlin/br/com/zup/autores/CadastrarAutorController.kt
+++ src/main/kotlin/br/com/zup/autores/CadastrarAutorController.kt
@@ -3,12 +3,15 @@ package br.com.zup.autores
 import io.micronaut.http.annotation.Body
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Post
+import io.micronaut.validation.Validated
+import javax.validation.Valid
 
 
+@Validated //Validação do DTO
 @Controller("/autores")
 class CadastrarAutorController {
     @Post
-    fun cadastra(@Body request: NovoAutorRequest){
+    fun cadastra(@Body @Valid request: NovoAutorRequest){
         println(request)
     }
 }
--- src/main/kotlin/br/com/zup/autores/NovoAutorRequest.kt
+++ src/main/kotlin/br/com/zup/autores/NovoAutorRequest.kt
@@ -1,7 +1,12 @@
 package br.com.zup.autores
 
+import io.micronaut.core.annotation.Introspected
+import javax.validation.constraints.NotBlank
+
+
+@Introspected //Validação dos Campos
 data class NovoAutorRequest(
-    val nome: String,
-    val email: String,
-    val descricao: String
+    @field:NotBlank val nome: String,
+    @field:NotBlank val email: String,
+    @field:NotBlank val descricao: String
 )
